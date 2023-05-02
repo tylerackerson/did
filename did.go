f587baa76dd73d5959915a81bfdcd009ad886896
tyler ackerson <tyler.a.ackerson@gmail.com>
2023-05-02 14:38:23 -0700

    Adds New, DidFromString, Length

--- did.go
+++ did.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
+	"github.com/pkg/errors"
 )
 
 const DefaultSeparator = "-"
@@ -15,8 +16,34 @@ type Did struct {
 	hex       string
 }
 
+// New creates a randomly-generated did with the provided prefix and default separator.
+// Prefix strings must be two characters.
+func New(prefix string) (*Did, error) {
+	if len(prefix) != 2 {
+		return nil, fmt.Errorf("invalid prefix '%s'", prefix)
+	}
+
+	u, err := uuid.NewRandom()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create did")
+	}
+
+	did, err := DidFromUuid(u, prefix)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create did")
+	}
+
+	return did, nil
+}
+
+// DidFromUuid creates a did from a UUID, the provided prefix, and default separator.
+// Prefix strings must be two characters.
 func DidFromUuid(uuid uuid.UUID, prefix string) (*Did, error) {
-	// TODO: validate
+	if len(prefix) != 2 {
+		return nil, fmt.Errorf("invalid prefix '%s'", prefix)
+	}
+	// TODO: more validations
+
 	hex := strings.ReplaceAll(uuid.String(), "-", "")
 	return &Did{
 		prefix:    prefix,
@@ -25,6 +52,27 @@ func DidFromUuid(uuid uuid.UUID, prefix string) (*Did, error) {
 	}, nil
 }
 
+// DidFromString creates a did from a string.
+// Basic validation is performed to ensure the did is correctly formatted.
+func DidFromString(s string) (*Did, error) {
+	parts := strings.Split(s, DefaultSeparator)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid did string '%s'", s)
+	}
+
+	return &Did{
+		prefix:    parts[0],
+		separator: DefaultSeparator,
+		hex:       parts[1],
+	}, nil
+}
+
+// String returns the string representation of a did.
 func (d Did) String() string {
 	return fmt.Sprintf("%s%s%s", d.prefix, d.separator, d.hex)
+}
+
+// Length returns the integer length of a did, including the prefix, separator, and hex.
+func (d Did) Length() int {
+	return len(d.String())
 }
--- did_test.go
+++ did_test.go
@@ -8,21 +8,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var u uuid.UUID
-var d Did
+func TestNewValidPrefix(t *testing.T) {
+	prefixes := []string{"ab", "XX", "19", "%$"} // special characters ok for now
+	for _, pr := range prefixes {
+		_, err := New(pr)
+		require.NoError(t, err)
+	}
+}
 
-func TestDidFromUuid(t *testing.T) {
-	u = uuid.New()
-	did, err := DidFromUuid(u, "us")
-	require.NoError(t, err)
-	require.NotEmpty(t, did)
-
-	d = *did
+func TestNewInvalidPrefix(t *testing.T) {
+	prefixes := []string{"abc", "a", "", "_"} // we only validate length for now
+	for _, pr := range prefixes {
+		_, err := New(pr)
+		require.Error(t, err)
+	}
 }
 
 func TestString(t *testing.T) {
-	uStr := strings.ReplaceAll(u.String(), "-", "")
-	expected := "us" + DefaultSeparator + uStr
-	didStr := d.String()
-	require.Equal(t, expected, didStr)
+	prefix := "ab"
+	d, _ := New(prefix)
+	s := d.String()
+	require.True(t, strings.HasPrefix(s, prefix))
+	require.Equal(t, 1, strings.Count(s, DefaultSeparator))
+}
+
+func TestDidFromString(t *testing.T) {
+	d, _ := New("ab")
+	_, err := DidFromString(d.String())
+	require.NoError(t, err)
+}
+
+func TestDidFromUuid(t *testing.T) {
+	u := uuid.New()
+	d, err := DidFromUuid(u, "us")
+	require.NoError(t, err)
+	require.NotEmpty(t, d)
 }
