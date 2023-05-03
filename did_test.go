724e1700bfed555912a346d1e1b713706e6e62cb
tyler ackerson <tyler.a.ackerson@gmail.com>
2023-05-03 10:02:39 -0700

    Adds tests for prefix validations

--- did_test.go
+++ did_test.go
@@ -9,18 +9,31 @@ import (
 )
 
 func TestNewValidPrefix(t *testing.T) {
-	prefixes := []string{"ab", "XX", "19", "%$"} // special characters ok for now
-	for _, pr := range prefixes {
+	prefixes := map[string]string{
+		"ab":  "two alpha chars",
+		"abc": "three alpha chars",
+		"AZ":  "uppercase",
+		"AbC": "upper and lowercase combined",
+	}
+
+	for pr, desc := range prefixes {
 		_, err := New(pr)
-		require.NoError(t, err)
+		require.NoError(t, err, desc)
 	}
 }
 
 func TestNewInvalidPrefix(t *testing.T) {
-	prefixes := []string{"abc", "a", "", "_"} // we only validate length for now
-	for _, pr := range prefixes {
+	prefixes := map[string]string{
+		"abcd": "too many chars",
+		"a":    "not enough chars",
+		"":     "empty",
+		".$":   "has special chars",
+		"k9":   "has numbers",
+	}
+
+	for pr, desc := range prefixes {
 		_, err := New(pr)
-		require.Error(t, err)
+		require.Error(t, err, desc)
 	}
 }
 
