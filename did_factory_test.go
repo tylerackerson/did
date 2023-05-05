f732ab5632e2664ed7d1060e12f8611a72b36bc3
tyler ackerson <tyler.a.ackerson@gmail.com>
2023-05-04 18:16:01 -0700

    Cleans up tests

--- did_factory_test.go
+++ did_factory_test.go
@@ -7,76 +7,102 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewValidPrefixDefault(t *testing.T) {
-	prefixes := map[string]string{
-		"ab":  "two alpha chars",
-		"abc": "three alpha chars",
-		"AZ":  "uppercase",
-		"AbC": "upper and lowercase combined",
-	}
+var validPrefixes = map[string]string{
+	"ab":  "two alpha chars",
+	"abc": "three alpha chars",
+	"AZ":  "uppercase",
+	"AbC": "upper and lowercase combined",
+}
 
-	for pr, desc := range prefixes {
+var invalidPrefixes = map[string]string{
+	"abcd": "too many chars",
+	"a":    "not enough chars",
+	"":     "empty",
+	".$":   "has special chars",
+	"k9":   "has numbers",
+}
+
+var invalidStrs = map[string]string{
+	"ab-526cac35b-e74429beb4f2ecca5-6c57":  "more than 1 separator",
+	"a9-526cac35b7e74429beb4f2ecca56c571":  "prefix invalid",
+	"ab=526cac35b7e74429beb4f2ecca56c571":  "separator invalid",
+	"ab-526cac357e74429beb4f2ecca56c571":   "hex has not enough chars",
+	"ab-526cac35b7e74429beb4f2ecca56c5711": "hex has too many chars",
+}
+
+var validSeparators = []string{"_", "-", "+", ""}
+var invalidSeparators = []string{"%", "--", "  ", "^"}
+
+func TestNewValidPrefixDefault(t *testing.T) {
+	for pr, desc := range validPrefixes {
 		_, err := New(pr)
 		require.NoError(t, err, desc)
 	}
 }
 
 func TestNewInvalidPrefixDefault(t *testing.T) {
-	prefixes := map[string]string{
-		"abcd": "too many chars",
-		"a":    "not enough chars",
-		"":     "empty",
-		".$":   "has special chars",
-		"k9":   "has numbers",
-	}
-
-	for pr, desc := range prefixes {
+	for pr, desc := range invalidPrefixes {
 		_, err := New(pr)
 		require.Error(t, err, desc)
 	}
 }
 
 func TestDidFromValidStringDefault(t *testing.T) {
-	d, _ := New("ab")
-	d, err := FromString(d.String())
-	require.NoError(t, err, d)
+	for pr, desc := range validPrefixes {
+		d, _ := New(pr)
+		d, err := FromString(d.String())
+		require.NoError(t, err, desc)
+	}
 }
 
 func TestDidFromInvalidStringDefaultg(t *testing.T) {
-	strs := map[string]string{
-		"ab-526cac35b-e74429beb4f2ecca5-6c57":  "more than 1 separator",
-		"a9-526cac35b7e74429beb4f2ecca56c571":  "prefix invalid",
-		"ab_526cac35b7e74429beb4f2ecca56c571":  "separator invalid",
-		"ab-526cac357e74429beb4f2ecca56c571":   "hex has not enough chars",
-		"ab-526cac35b7e74429beb4f2ecca56c5711": "hex has too many chars",
-	}
-
-	for s, desc := range strs {
+	for s, desc := range invalidStrs {
 		_, err := FromString(s)
 		require.Error(t, err, desc)
 	}
 }
 
 func TestDidFromUuidDefault(t *testing.T) {
-	u := uuid.New()
-	d, err := FromUuid(u, "us")
-	require.NoError(t, err)
-	require.NotEmpty(t, d)
-}
-
-func TestNewDidFactoryValid(t *testing.T) {
-	prefixes := []string{"_", "-", "+", ""}
-	for _, p := range prefixes {
-		_, err := NewDidFactory("us", p)
-		require.NoError(t, err)
+	for pr, desc := range validPrefixes {
+		u := uuid.New()
+		d, err := FromUuid(u, pr)
+		require.NoError(t, err, desc)
+		require.NotEmpty(t, d)
 	}
 }
 
-func TestNewDidFactoryInvalid(t *testing.T) {
-	prefixes := []string{"%", "--", "  ", "^"}
-	for _, p := range prefixes {
-		_, err := NewDidFactory("us", p)
-		require.Error(t, err)
+func TestNewDidFactoryValid(t *testing.T) {
+	for _, s := range validSeparators {
+		for pr := range validPrefixes {
+			_, err := NewDidFactory(pr, s)
+			require.NoError(t, err)
+		}
+	}
+}
+
+func TestNewDidFactoryInvalidSeparator(t *testing.T) {
+	for _, s := range invalidSeparators {
+		for pr := range validPrefixes {
+			_, err := NewDidFactory(pr, s)
+			require.Error(t, err)
+		}
+		for pr := range invalidPrefixes {
+			_, err := NewDidFactory(pr, s)
+			require.Error(t, err)
+		}
+	}
+}
+
+func TestNewDidFactoryInvalidPrefix(t *testing.T) {
+	for _, s := range invalidSeparators {
+		for pr := range validPrefixes {
+			_, err := NewDidFactory(pr, s)
+			require.Error(t, err)
+		}
+		for pr := range invalidPrefixes {
+			_, err := NewDidFactory(pr, s)
+			require.Error(t, err)
+		}
 	}
 }
 
@@ -88,6 +114,7 @@ func TestFactoryNewDid(t *testing.T) {
 }
 
 func TestFactoryDidFromValidString(t *testing.T) {
+	// for now, DidFactory does not validate  for prefix or separator matches
 	df, _ := NewDidFactory("us", "_")
 	d, _ := df.NewDid()
 	d, err := df.DidFromString(d.String())
@@ -95,7 +122,11 @@ func TestFactoryDidFromValidString(t *testing.T) {
 }
 
 func TestFactoryDidFromInvalidString(t *testing.T) {
-	// TODO
+	df, _ := NewDidFactory("us", "_")
+	for str, desc := range invalidStrs {
+		_, err := df.DidFromString(str)
+		require.Error(t, err, desc)
+	}
 }
 
 func TestFactoryDidFromUuid(t *testing.T) {
